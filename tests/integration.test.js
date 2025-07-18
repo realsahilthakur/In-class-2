4fa7a0c1680146906e5c11572dc0230b9bec01da
Sahil Thakur <90372772+realsahilthakur@users.noreply.github.com>
2025-07-17 21:56:57 -0400

    task 3

--- tests/integration.test.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-
-describe('Integration Test', () => {
-  it('should return 200 from service container', async () => {
-    const response = await axios.get('http://localhost:3000');
-    expect(response.status).toBe(200);
-  });
-});
